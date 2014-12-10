0181755182926b51838fbb14c9dd95d73631de41
Santiago Palladino <spalladino@gmail.com>
2014-12-10 21:46:07 +0300

    Initial form and request to mbuilder with testing account

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+var USERNAME = "katukakaunda@gmail.com";
+var MBUILDER_TRIGGER_URL = "http://mbuilder.instedd.org/external/application/150/trigger/subscribers?";
+
 $(function() {
-  alert("Hello world");
+
+  $('form').on('submit', function(evt) {
+    evt.preventDefault();
+
+    var message = $('#message').val();
+    var location = $('#location').val();
+    var role = $('#role').val();
+    var password = $('#password').val();
+
+    var url = MBUILDER_TRIGGER_URL + 'location=' + location + '&message=' + message + '&phone=000';
+
+    $.ajax({
+      url: url,
+      type: "GET",
+      username: USERNAME,
+      password: password,
+      success: function(data) {
+        alert("Success!");
+        console.log(data);
+      }
+    });
+
+  });
+
 });
