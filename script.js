2598d166d42f5d010ba8955fa5538b6ac6458ae2
Santiago Palladino <spalladino@gmail.com>
2014-12-11 00:03:50 +0300

    Refactored URL for request and moved submit to another function

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
+// Main function run on document load
 $(function() {
 
   $(document).on('submit', 'form', function(evt) {
-
     evt.preventDefault();
-
-    var message = $('#message').val();
-    var location = $('#location').val();
-    var role = $('#role').val();
-    var password = $('#password').val();
-
-    var url = MBUILDER_URL + 'external/application/' + MBUILDER_APP_ID + '/trigger/' + MBUILDER_TRIGGER;
-    url += '?location=' + location + '&message=' + message + '&phone=000';
-
-    $.ajax({
-      url: url,
-      type: "POST",
-      dataType: 'json',
-      contentType: 'text/plain',
-      content: '',
-      xhrFields: {
-        withCredentials: false
-      },
-      headers: {
-        "Authorization": "Basic " + btoa(USERNAME + ":" + password)
-      },
-      success: function(data) {
-        alert("Success!");
-        console.log(data);
-      },
-      error: function(err) {
-        alert("Failure");
-        console.log(err);
-      }
-    });
-
+    submitForm();
   });
 
+  loadLocations();
+
 });
+
+// Submits form by sending message to mBuilder
+function submitForm() {
+  var password = $('#password').val();
+  var data = {
+    message: $('#message').val(),
+    location: $('#location').val(),
+    role: $('#role').val()
+  };
+
+  var url = MBUILDER_URL
+    + 'external/application/' + MBUILDER_APP_ID
+    + '/trigger/' + MBUILDER_TRIGGER
+    + '?' + $.param(data);
+
+  $.ajax({
+    url: url,
+    type: "POST",
+    dataType: 'json',
+    contentType: 'text/plain',
+    content: '',
+    xhrFields: {
+      withCredentials: false
+    },
+    headers: {
+      "Authorization": "Basic " + btoa(USERNAME + ":" + password)
+    },
+    success: function(data) {
+      alert("Success!");
+      console.log(data);
+    },
+    error: function(err) {
+      alert("Failure");
+      console.log(err);
+    }
+  });
+};
+
+// Loads locations from contacts table in mbuilder
+function loadLocations() {
+  if (!typeof(MBUILDER_CONTACTS) === 'undefined') {
+    $.get(MBUILDER_CONTACTS, {
+
+    });
+  }
+};
