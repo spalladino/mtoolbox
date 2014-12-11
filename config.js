2dc6d15d8b777d2a3e5e42b155c394014279716d
Santiago Palladino <spalladino@gmail.com>
2014-12-11 16:08:36 +0300

    Corrected broadcast trigger name

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 USERNAME = "epihack.fasttrack@gmail.com";
-MBUILDER_TRIGGER = 'deliver';
+MBUILDER_TRIGGER = 'Broadcast';
 MBUILDER_APP_ID = 161;
 MBUILDER_URL = "http://mbuilder.instedd.org/";
 MBUILDER_CONTACTS_TABLE = "Contacts";
