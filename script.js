45853a2317462a9720859098a897f5049ee3aa4c
Santiago Palladino <spalladino@gmail.com>
2014-12-11 22:54:06 +0300

    Stabilised commit

--- script.js
+++ script.js
@@ -5,55 +5,86 @@ $(function() {
     // Handle form submit by sending message to mbuilder
     .on('submit', 'form', function(evt) {
       evt.preventDefault();
-      submitForm();
+      $('.alert').alert('close');
+      $('#submitConfirmModal').modal('show');
     })
-    // Reload locations on change of password
+    // Reload items on change of password
     .on('change', '#password', function(evt) {
-      loadFromMbuilder();
+      loadData();
     })
     // or on manual reload
     .on('click', '.reload-dropdown', function(evt) {
       evt.preventDefault();
-      loadFromMbuilder();
+      loadData();
     })
     // Clear form when clear button is clicked
     .on('click', '#clear', function(evt) {
       evt.preventDefault();
-      // Clear form here!
-      $('#password').text('');
-      $('#location').text('');
-      $('#role').text('');
       $('#message').text('');
     })
-
-    .on('click', '#send', function(evt) {
+    // Confirmation from modal send box
+    .on('click', '#sendMessage', function(evt) {
       evt.preventDefault();
-      //$('#exampleModal').modal('hide');
-      alert("Testing");
+      $(this).button('sending');
+      doSubmit();
     });
-
-    //modal box
-    /*$('#exampleModal').on('show.bs.modal', function (event) {
-      var button = $(event.relatedTarget) // Button that triggered the modal
-      var recipient = button.data('whatever') // Extract info from data-* attributes
-      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-      var modal = $(this)
-      modal.find('.modal-title').text('New message to ' + recipient)
-      modal.find('.modal-body input').val(recipient)
-    });*/
 
   // Also try to load if password is filled by browser
   window.setTimeout(function() {
-    loadFromMbuilder();
+    loadData();
   }, 500);
 
+  // Set contents based on config
+  $('#mbuilderContacts').attr('href', MBUILDER_URL+'applications/'+MBUILDER_APP_ID+'/data/')
+  $('#password').attr('placeholder', 'Password for ' + USERNAME);
 });
 
 
-// Submits form by sending message to mBuilder
-function submitForm() {
+// Loads contacts table in mbuilder
+function loadData() {
   var password = $('#password').val();
+  if (password == '') return;
+
+  var url = MBUILDER_URL
+    + 'api/applications/'
+    + MBUILDER_APP_ID
+    + '/tables/'
+    + MBUILDER_CONTACTS_TABLE;
+
+  $.ajax({
+    url: url,
+    type: "GET",
+    dataType: 'json',
+    headers: {
+      "Authorization": "Basic " + btoa(USERNAME + ":" + password)
+    },
+    success: function(contacts) {
+      loadDropdown(contacts, 'Location', $('#location'));
+      loadDropdown(contacts, 'Role', $('#role'));
+      loadDropdown(contacts, 'Language', $('#lang'));
+    }
+  });
+};
+
+function loadDropdown(contacts, field, dropdown) {
+  var items = _.pluck(contacts, field).sort();
+  items = _.uniq(items, true);
+  dropdown.empty();
+  _.each(items, function(item) {
+    $('<option>' + item + '</option>').appendTo(dropdown);
+  });
+};
+
+// Show an alert
+function showAlert(content, klazz) {
+  var elem = $('<div class="alert alert-' + klazz + ' alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>' + content + '</div>');
+  elem.insertAfter('.mtoolbox');
+}
+
+// Submit form to mbuilder and send data
+function doSubmit() {
+  var password = $('#password').val();
+
   var data = {
     message: $('#message').val(),
     location: $('#location').val(),
@@ -66,61 +97,6 @@ function submitForm() {
     + '/trigger/' + MBUILDER_TRIGGER
     + '?' + $.param(data);
 
-  confirmSubmit(data, url, password);
-};
-
-// Loads contacts table in mbuilder
-function loadFromMbuilder() {
-  var password = $('#password').val();
-  if (password == '') return;
-
-  var url = MBUILDER_URL + 'api/applications/' + MBUILDER_APP_ID + '/tables/' + MBUILDER_CONTACTS_TABLE;
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: 'json',
-    headers: {
-      "Authorization": "Basic " + btoa(USERNAME + ":" + password)
-    },
-    success: function(contacts) {
-      extractAndLoad(contacts, 'Location', $('#location'));
-      extractAndLoad(contacts, 'Role', $('#role'));
-      extractAndLoad(contacts, 'Language', $('#lang'));
-    }
-  });
-};
-
-function extractAndLoad(contacts, field, dropdown) {
-  var items = _.pluck(contacts, field).sort();
-  items = _.uniq(items, true);
-  dropdown.empty();
-  _.each(items, function(item) {
-    $('<option>' + item + '</option>').appendTo(dropdown);
-  });
-};
-
-
-// Confirmation before submitting the message
-function confirmSubmit(data,url,password){
-  /*$("#response-message").text('Are you sure you want to send the message?');
-  $("#dialog-confirm").dialog({
-    resizable: false,
-    height:180,
-    modal: true,
-    buttons: {
-      "Ok": function() {
-        ajaxSubmit(data,url,password);
-      },
-      "Cancel":function(){
-        $(this).dialog("close");
-      }
-    }
-  });*/
-  //$('#exampleModal').modal('show');
-}
-
-//Ajax submit
-function ajaxSubmit(data,url,password) {
   $.ajax({
     url: url,
     type: "POST",
@@ -134,33 +110,25 @@ function ajaxSubmit(data,url,password) {
       "Authorization": "Basic " + btoa(USERNAME + ":" + password)
     },
     success: function(data) {
-      //$('#message').val('');
-      /*$('#response-message').text(data.length+" messages have been sent sucessfully!");
-      $("#dialog-confirm").dialog({
-        resizable: false,
-        height:180,
-        modal: true,
-        buttons: {
-          "Ok": function() {
-            $(this).dialog("close");
-          }
-        }
-      });*/
-      $('#exampleModal').modal('show');
+      $('#submitConfirmModal').modal('hide');
+      $('#sendMessage').button('reset');
+
+      var msg = null;
+      if (data.length == 0) {
+        msg = "No messages have been sent";
+      } else if (data.length == 1) {
+        msg = "One message has been sent";
+      } else {
+        msg = data.length + " messages have been sent";
+      }
+
+      showAlert(msg, 'success');
     },
     error: function(err) {
       console.log(err);
-      $('#response-message').text('Submit failed. Your password/username is incorrect.');
-      $("#dialog-confirm").dialog({
-        resizable: false,
-        height:180,
-        modal: true,
-        buttons: {
-          "Ok": function() {
-            $(this).dialog("close");
-          }
-        }
-      });
+      $('#submitConfirmModal').modal('hide');
+      $('#sendMessage').button('reset');
+      showAlert("There was an error sending your message. Please check your credentials and try again.", 'danger');
     }
   });
 };
