0c4a6c90cf9b9a3e1248147d9727c54dfefbdf45
Santiago Palladino <spalladino@gmail.com>
2014-12-11 14:08:01 +0300

    Reorganised code and corrected ajax request

--- script.js
+++ script.js
@@ -9,17 +9,22 @@ $(function() {
     })
     // Reload locations on change of password
     .on('change', '#password', function(evt) {
-      loadLocations();
+      loadFromMbuilder();
     })
     // or on manual reload
     .on('click', '.reload-dropdown', function(evt) {
       evt.preventDefault();
-      loadLocations();
+      loadFromMbuilder();
+    })
+    // Clear form when clear button is clicked
+    .on('click', '#clear', function(evt) {
+      evt.preventDefault();
+      // Clear form here!
     });
 
   // Also try to load if password is filled by browser
   window.setTimeout(function() {
-    loadLocations();
+    loadFromMbuilder();
   }, 500);
 
 });
@@ -39,64 +44,15 @@ function submitForm() {
     + '/trigger/' + MBUILDER_TRIGGER
     + '?' + $.param(data);
 
-    var confirmation = confirmSubmit(data,url,password);
-
-    //if(!confirm('Are you sure?')){
-      //  return;
-   // }
-
-  /*$.ajax({
-    url: url,
-    type: "POST",
-    dataType: 'json',
-    contentType: 'text/plain',
-    content: '',
-    xhrFields: {
-      withCredentials: false
-    },
-    headers: {
-      "Authorization": "Basic " + btoa(USERNAME + ":" + password)
-    },
-    success: function(data) {
-      $('#message').val('');
-      $('#response-message').text(data.length+" messages have been sent sucessfully!");
-      $("#dialog-confirm").dialog({
-        resizable: false,
-        height:180,
-        modal: true,
-        buttons: {
-          "Ok": function() {
-            $(this).dialog("close");
-          }
-        }
-      });
-    },
-    error: function(err) {
-      console.log(err);
-      $('#response-message').text('Submit failed. Your password/username is incorrect.');
-      $("#dialog-confirm").dialog({
-        resizable: false,
-        height:180,
-        modal: true,
-        buttons: {
-          "Ok": function() {
-            $(this).dialog("close");
-          }
-        }
-      });
-    }
-  });*/
+  confirmSubmit(data, url, password);
 };
 
-// Loads locations from contacts table in mbuilder
-function loadLocations() {
+// Loads contacts table in mbuilder
+function loadFromMbuilder() {
   var password = $('#password').val();
   if (password == '') return;
 
-  var url = 'http://mbuilder.instedd.org/api/applications/' + MBUILDER_APP_ID + '/tables/' + MBUILDER_CONTACTS_TABLE;
-  $.get(url, null, function(data) {
-    
-
+  var url = MBUILDER_URL + 'api/applications/' + MBUILDER_APP_ID + '/tables/' + MBUILDER_CONTACTS_TABLE;
   $.ajax({
     url: url,
     type: "GET",
@@ -104,50 +60,44 @@ function loadLocations() {
     headers: {
       "Authorization": "Basic " + btoa(USERNAME + ":" + password)
     },
-    success: function(data) {
-      extractAndLoad('Location', $('#location'));
-      extractAndLoad('Role', $('#role'));
+    success: function(contacts) {
+      extractAndLoad(contacts, 'Location', $('#location'));
+      extractAndLoad(contacts, 'Role', $('#role'));
     }
-
   });
+};
 
-});
-}
-function extractAndLoad(field, dropdown) {
-    var items = _.pluck(data, field).sort();
-    items = _.uniq(items, true);
-    dropdown.empty();
-    _.each(items, function(item) {
-      $('<option>' + item + '</option>').appendTo(dropdown);
-    });
-  }
+function extractAndLoad(contacts, field, dropdown) {
+  var items = _.pluck(contacts, field).sort();
+  items = _.uniq(items, true);
+  dropdown.empty();
+  _.each(items, function(item) {
+    $('<option>' + item + '</option>').appendTo(dropdown);
+  });
+};
 
-//handle tabs
- $(function() {
-$( "#tabs" ).tabs();
-});
 
- //confirmation
- function confirmSubmit(data,url,password){
-    $("#response-message").text('Are you sure you want to send the message?');
-    $("#dialog-confirm").dialog({
-      resizable: false,
-      height:180,
-      modal: true,
-      buttons: {
-        "Ok": function() {
-          ajaxSubmit(data,url,password);
-        },
-        "Cancel":function(){
-          $(this).dialog("close");
-        }
+// Confirmation before submitting the message
+function confirmSubmit(data,url,password){
+  $("#response-message").text('Are you sure you want to send the message?');
+  $("#dialog-confirm").dialog({
+    resizable: false,
+    height:180,
+    modal: true,
+    buttons: {
+      "Ok": function() {
+        ajaxSubmit(data,url,password);
+      },
+      "Cancel":function(){
+        $(this).dialog("close");
       }
-    });
- }
+    }
+  });
+}
 
- //Ajax submit
- function ajaxSubmit(data,url,password){
-    $.ajax({
+//Ajax submit
+function ajaxSubmit(data,url,password) {
+  $.ajax({
     url: url,
     type: "POST",
     dataType: 'json',
@@ -188,5 +138,5 @@ $( "#tabs" ).tabs();
       });
     }
   });
- }
+};
 
